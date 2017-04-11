0825f84bd824e49e01fdd5bb4857cf02fe483220
Chuan Ning <cnin0770@Avalanche.local>
2017-04-11 14:04:21 +1000

    3rd final

--- a1.js
+++ a1.js
@@ -1,12 +1,13 @@
 function titleSearchFunction() {
 
-	var input, filter, table, tr, td, i;
+	var input, filter, table, tr, td, i, k;
 
 	input = document.getElementById("titleSearch");
 	filter = input.value.toUpperCase();
 	table = document.getElementById("paintings");
 	//tr = table.getElementByTagName("tr");
 	tr = document.querySelectorAll(".pp");
+	k = 0;
 
 	//loop
 	for (i=0; i<tr.length; i++) {
@@ -14,13 +15,20 @@ function titleSearchFunction() {
 		td = tr[i].querySelectorAll(".tt")[0];
 
 		if (td) {
-			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+			if (td.innerHTML.toUpperCase().indexOf(filter) > -1 & filter != '') {
 				//tr[i].style.display = "";
-				tr[i].style.backgroundColor="green";
+				tr[i].style.backgroundColor="#87CEFA";
+				tr[i].style.fontWeight='bold';
+
+				k = k + 1;
 			} //else {
 				//tr[i].style.display = "none";
 			//}
-		}
+		} 
+	}
+
+	if (k == 0 & filter != '') {
+		alert("No matching result.");
 	}
 }
 
@@ -34,17 +42,24 @@ function genreSearchFunction() {
 	//tr = table.getElementByTagName("tr");
 	tr = document.querySelectorAll(".pp");
 
+
+
 	//loop
 	for (i=0; i<tr.length; i++) {
 		//td = tr[i].getElementByTagName("td")[0];
 		td = tr[i].querySelectorAll(".gg")[0];
 
 		if (td) {
-			if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+			if (filter == "GENRE" || td.innerHTML.toUpperCase().indexOf(filter) > -1) {
 				tr[i].style.display = "";
 			} else {
 				tr[i].style.display = "none";
-			}
+			} 
 		}
 	}
+
 }
+
+//function renderAction() {
+//	event.preventDefault();
+//}
